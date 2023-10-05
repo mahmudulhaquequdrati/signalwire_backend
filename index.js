bbedeaee265839ac9e2d1ebc80d85ab6d5d7acd0
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-10-06 03:19:02 +0600

    server configued

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const colors = require("colors");
+const { Voice } = require("@signalwire/realtime-api");
 // const path = require("path");
 const cors = require("cors");
 const connectDB = require("./src/config/connectDB");
@@ -22,6 +23,25 @@ app.get("/", (req, res) => {
   );
 });
 
+const client = new Voice.Client({
+  project: "5c46b95f-6635-4c38-a973-e3004fb9d2ac",
+  token: "PT570e119c94975673ab1416ca666b3c319ed82dc0a182c541",
+  host: "https://vultik.signalwire.com/api",
+  topics: ["office"],
+});
+console.log("yes");
+client.on("call.received", async (call) => {
+  console.log("Got call", call.from, call.to);
+
+  try {
+    // await call.answer();
+    console.log("Inbound call answered");
+
+    // await call.playTTS({ text: "Welcome to SignalWire!" });
+  } catch (error) {
+    console.error("Error answering inbound call", error);
+  }
+});
 // --------------------
 // ---------------
 // ----------
@@ -44,6 +64,7 @@ mongoose.connection.once("open", () => {
     colors.green.underline(`📗Connected`),
     colors.yellow.underline("to Server!")
   );
+
   app.listen(PORT, () => console.log(`Server running in port no : ${PORT}`));
 });
 mongoose.connection.on("error", (err) => {
